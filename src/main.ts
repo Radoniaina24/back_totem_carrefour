11a3e3ce529a76df208a3e84f38183e1e6f55e81
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-11-02 01:21:08 +0300

    v1.7

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { EventEmitter } from 'events'; // ✅ importer EventEmitter
+import { EventEmitter } from 'events'; //importer EventEmitter
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   //  Augmenter la limite globale
   EventEmitter.defaultMaxListeners = 20; // par exemple, 20 listeners au lieu de 10
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Active cookie-parser pour lire les cookies dans les requêtes
   app.use(cookieParser());
+
+  app.set('trust proxy', 1);
 
   // Important : activer CORS pour que le frontend puisse envoyer les cookies
   app.enableCors({
