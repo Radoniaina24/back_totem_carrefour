eb663048d2d85622deaef7d0850cea04a0a4c265
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-11-02 01:58:34 +0300

    v1.8

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -30,7 +30,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // secure uniquement en prod
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // important pour HTTPS
-        maxAge: 24 * 60 * 60 * 1000, // 1 jour
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
         path: '/', // assure la disponibilité sur tout le domaine
       });
 
